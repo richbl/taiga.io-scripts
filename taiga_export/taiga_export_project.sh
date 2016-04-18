322d856dceb0a6fa121396983f9f173f8503ea6f
Rich Bloch <richbl@gmail.com>
2016-04-17 19:31:49 -0700

    Refactor
    Refactored using A-Bash-Template (BaT) template. Adde support to process HTML status codes from Taiga server.

--- taiga_export/taiga_export_project.sh
+++ taiga_export/taiga_export_project.sh
@@ -17,204 +17,136 @@ PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 # -----------------------------------------------------------------------------
 #
 # A bash script to GET a JSON export file from a Taiga project
+# version: 0.2.0
 #
-# Version: 0.1
+# requirements:
+#  --preexisting Taiga project
+#  --jq program (https://stedolan.github.io/jq/) installed: used to parse /data/config.json
+#  --curl program (http://curl.haxx.se/)
 #
-# Requirements:
+# inputs:
+#  --website URL (e.g., http://www.website.com)
+#  --project slug name (different than project name)
+#  --output directory to save JSON file
+#  --username (must have appropriate site admin permissions to export project)
+#  --password
 #
-#  --Preexisting Taiga project
-#  --Curl (http://curl.haxx.se/) must be installed on host machine
-#  --JQ (https://stedolan.github.io/jq/) must be installed on host machine
+# outputs:
+#  --notification of script success/failure
+#  --side effect: an exported JSON file
 #
-# Inputs:
-#
-#  --Website URL (e.g., http://www.website.com)
-#  --Project slug name (based on project name)
-#  --Output directory to save exported file (absolute path)
-#  --Username (must have appropriate admin permissions to export project)
-#  --Password
-#
-# Outputs:
-#
-#  --An exported JSON file if processed successful
-#  --If failure, causing error message displayed
-#
-
-echo "
-|
-| A bash script to GET a JSON export file from a Taiga project
-|
-| Usage:
-|   export_taiga [options]
-|
-|   -w, --website          website_url (e.g., http://www.website.com)
-|   -n, --projectslugname  project_slug_name (not the project name)
-|   -o, --outputdir        absolute directory path for exported file
-|   -u, --username         user_name
-|   -p, --password         password
-|"
-echo
 
 # -----------------------------------------------------------------------------
-# Functions
+# script declarations
 #
+shopt -s extglob
+EXEC_DIR="$(dirname "$0")"
+. ${EXEC_DIR}/lib/args
 
-function quit {
-  echo
-  exit 1
-}
+ARGS_FILE="${EXEC_DIR}/data/config.json"
 
+declare -a REQ_PROGRAMS=('jq' 'curl')
 DEBUG=false
 
 # -----------------------------------------------------------------------------
-# Scan cmdline for arguments
+# perform script configuration, arguments parsing, and validation
 #
-while [[ $# -gt 0 ]]
-do
-  ARG="$1"
-
-  case $ARG in
-    -w|--website)
-      ARG_WEBSITE="$2"
-      shift # skip argument
-      ;;
-    -n|--projectslugname)
-      ARG_PROJECTSLUG="$2"
-      shift # skip argument
-      ;;
-    -o|--outputdir)
-      ARG_OUTPUTDIR="$2"
-      shift # skip argument
-      ;;
-    -u|--username)
-      ARG_USERNAME="$2"
-      shift # skip argument
-      ;;
-    -p|--password)
-      ARG_PASSWORD="$2"
-      shift # skip argument
-      ;;
-      *)
-      # unknown argument
-      ;;
-  esac
-  shift # skip argument or value
-done
-
-# -----------------------------------------------------------------------------
-# check argument completeness
-#
-if [ -z "${ARG_WEBSITE}" ]; then
-  echo "Error: website_url argument (-w|--website) missing."
-  quit
-fi
-
-if [ -z "${ARG_PROJECTSLUG}" ]; then
-  echo "Error: project_slug_name argument (-n|--projectslugname) missing."
-  quit
-fi
-
-if [ -z "${ARG_OUTPUTDIR}" ]; then
-  echo "Error: outputdir argument (-o|--outputdir) missing."
-  quit
-fi
-
-if [ -z "${ARG_USERNAME}" ]; then
-  echo "Error: user_name argument (-u|--username) missing."
-  quit
-fi
-
-if [ -z "${ARG_PASSWORD}" ]; then
-  echo "Error: password argument (-p|--password) missing."
-  quit
-fi
-
-# -----------------------------------------------------------------------------
-# check requirements
-#
-if  ! type "curl" &> /dev/null; then
-  echo "Error: curl program not installed."
-  quit
-fi
-
-if ! type "jq" &> /dev/null; then
-  echo "Error: jq program not installed."
-  quit
-fi
+check_program_dependencies "REQ_PROGRAMS[@]"
+display_banner
+scan_for_args "$@"
+check_for_args_completeness
 
 # -----------------------------------------------------------------------------
 # Get AUTH_TOKEN
 #
-USER_AUTH_DETAIL=$( curl -X POST -H "Content-Type: application/json"\
-                    -d '{ "type": "normal", "username": "'${ARG_USERNAME}'", "password": "'${ARG_PASSWORD}'" }'\
-                    "${ARG_WEBSITE}"/api/v1/auth 2>/dev/null )
+USER_AUTH_DETAIL=$( curl -X POST \
+                    -H "Content-Type: application/json"\
+                    -d '{
+                        "type": "normal",
+                        "username": "'$(get_config_arg_value username)'",
+                        "password": "'$(get_config_arg_value password)'"
+                        }'\
+                    "$(get_config_arg_value website)"/api/v1/auth 2>/dev/null )
 
-AUTH_TOKEN=$( echo "${USER_AUTH_DETAIL}" | jq -r '.auth_token' )
+AUTH_TOKEN=$( printf "%s" "${USER_AUTH_DETAIL}" | jq -r '.auth_token' )
 
 if [ "${DEBUG}" = true ]; then
-  echo "AUTH_TOKEN is: ${AUTH_TOKEN}"
+  printf "%s\n" "AUTH_TOKEN is: ${AUTH_TOKEN}"
 fi
 
 # Exit if AUTH_TOKEN is not present (failed login)
 #
 if [ -z "${AUTH_TOKEN}" ]; then
-  echo "Error: Incorrect username and/or password supplied"
+  printf "%s\n" "Error: Incorrect username and/or password supplied"
   quit
 fi
 
 # -----------------------------------------------------------------------------
 # Get project ID
 #
-JSON_PROJECT_ID=$( curl -X GET -H "Content-Type: application/json"\
+JSON_PROJECT_ID=$( curl -X GET \
+                   -H "Content-Type: application/json"\
                    -H "Authorization: Bearer ${AUTH_TOKEN}"\
-                   "${ARG_WEBSITE}"/api/v1/resolver?project="${ARG_PROJECTSLUG}" 2>/dev/null )
+                   "$(get_config_arg_value website)"/api/v1/resolver?project="$(get_config_arg_value 'project slug name')" 2>/dev/null )
 
-
-PROJECT_ID=$( echo "${JSON_PROJECT_ID}" | jq -r '.project' )
+PROJECT_ID=$( printf "%s" "${JSON_PROJECT_ID}" | jq -r '.project' )
 
 if [ "${DEBUG}" = true ]; then
-  echo "PROJECT_ID is: ${PROJECT_ID}"
+  printf "%s\n" "PROJECT_ID is: ${PROJECT_ID}"
 fi
 
 if [ -z "${PROJECT_ID}" ]; then
-  echo "Error: Project ID not found."
+  printf "%s\n" "Error: Project ID not found."
   quit
 fi
 
 # -----------------------------------------------------------------------------
 # Export JSON file
 #
-# NOTE: taiga administrative permissions are required to export project file
+
+# determine if the Taiga server is configured to permit the return of an
+# exported JSON project object (HTML status code must be 200)
 #
+# For details, see Taiga REST API documentation:
+# https://taigaio.github.io/taiga-doc/dist/api.html#export-import-export-dump
+#
+HTML_STATUS_CODE=$( curl -s -o /dev/null -w "%{http_code}" -H "Content-Type: application/json"\
+            -H "Authorization: Bearer ${AUTH_TOKEN}"\
+            "$(get_config_arg_value website)"/api/v1/exporter/"${PROJECT_ID}" 2>/dev/null )
+
+if [ "${DEBUG}" = true ]; then
+  printf "%s\n" "HTML_STATUS_CODE is: ${HTML_STATUS_CODE}"
+fi
+
+if [ ${HTML_STATUS_CODE} -ne 200 ]; then
+  printf "%s\n" "Taiga server returned HTTP status code: ${HTML_STATUS_CODE}. Status code must be 200 to proceed."
+  quit
+fi
+
 JSON_URL=$( curl -X GET -H "Content-Type: application/json"\
             -H "Authorization: Bearer ${AUTH_TOKEN}"\
-            "${ARG_WEBSITE}/api/v1/exporter/${PROJECT_ID}" 2>/dev/null )
+            "$(get_config_arg_value website)"/api/v1/exporter/"${PROJECT_ID}" 2>/dev/null )
 
 if [ "${DEBUG}" = true ]; then
-  echo "JSON_URL is: ${JSON_URL}"
+  printf "%s\n" "JSON_URL is: ${JSON_URL}"
 fi
 
-JSON_FILE=$( echo "${JSON_URL}" | jq -r '.url' )
+JSON_FILE=$(printf "%s" "${JSON_URL}" | jq -r '.url')
 
 if [ "${DEBUG}" = true ]; then
-  echo "JSON_FILE is: ${JSON_FILE}"
+  printf "%s\n" "JSON_FILE is: ${JSON_FILE}"
 fi
 
-# Exit if JSON_FILE is not gotten
+# Exit if JSON_FILE is not acquired
 #
-if [ -z "${JSON_FILE}" ]; then
-  echo "${JSON_URL}"
-  echo "Error: JSON export failed."
+if [ "${JSON_FILE}" == "null" ]; then
+  printf "%s\n" "Error: JSON export failed."
   quit
 fi
 
 # Save JSON_FILE to directory specified
 #
-mkdir -p "${ARG_OUTPUTDIR}"
-
-RESULTS="${ARG_PROJECTSLUG}"-export-"$(date +"%Y%m%d%H%M%S")".json
-
-curl -s "${JSON_FILE}" > "${ARG_OUTPUTDIR}/${RESULTS}"
-
-echo "Success: JSON export completed. Results file (${RESULTS}) created in ${ARG_OUTPUTDIR}."
-echo
+mkdir -p "$(get_config_arg_value 'output directory')"
+RESULTS="$(get_config_arg_value 'project slug name')"-export-"$(date +"%Y%m%d%H%M%S")".json
+curl -s "${JSON_FILE}" > "$(get_config_arg_value 'output directory')/${RESULTS}"
+printf "%s\n\n" "Success: JSON export completed. Results file (${RESULTS}) created in $(get_config_arg_value 'output directory')."
